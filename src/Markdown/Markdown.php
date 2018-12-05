02f2f8db00e31de977640e1d0f33762cb834e1da
ocpu <martin.hovre@opencubes.io>
2018-12-05 13:01:23 +0100

    Removed comments

--- src/Markdown/Markdown.php
+++ src/Markdown/Markdown.php
@@ -41,37 +41,4 @@ class Markdown extends ServiceProvider
         $html = $this->parser->text($source);
         return new Content($data, $html);
     }
-
-    // function extract_post($path, $parser) {
-    //     $file = file_get_contents($path);
-    //     $data = null;
-    //     if (starts_with($file, "---")) {
-    //         preg_match("/---\n([\s\S]*)\n\.\.\.\n?/iuU", $file, $matches);
-    //         $file = str_replace($matches[0], "", $file);
-    //         $data = spyc_load($matches[1]);
-    //         while (preg_grep("/.+@(en|sv)/", array_keys($data))) {
-    //             $keys = preg_grep("/.+@(en|sv)/", array_keys($data));
-    //             preg_match("/(.+)@(en|sv)/", $keys[0], $match);
-    //             $data[$match[1]] = 
-    //                 $match[2] === "sv" && $GLOBALS["LANG"] === "sv" ? $data[$keys[0]] :
-    //                 $match[2] === "sv" && $GLOBALS["LANG"] === "en" ? $data[$keys[1]] :
-    //                 $match[2] === "en" && $GLOBALS["LANG"] === "sv" ? $data[$keys[1]] :
-    //                 $match[2] === "en" && $GLOBALS["LANG"] === "en" ? $data[$keys[0]] :
-    //                 ""
-    //             ;
-    //             unset($data[$keys[0]]);
-    //             unset($data[$keys[1]]);
-    //         }
-    //     }
-    
-    //     $languageContent = explode("<hr />", $parser->text($file));
-    //     $swedish = $languageContent[0];
-    //     $english = $languageContent[1];
-    //     $content = $GLOBALS["LANG"] === "sv" ? $swedish : $GLOBALS["LANG"] === "en" ? $english : "";
-    
-    //     return [
-    //         "content" => $content,
-    //         "data" => $data
-    //     ];
-    // }
 }
